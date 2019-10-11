6ac5f01988aae726cac90a56449d32e84e0f83e5
Isaiah Francois <francoisisaiaht@yahoo.com>
2019-10-11 11:34:18 -0400

    Removed my info

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "francoisisaiaht64@gmail.com",
-    pass: "Godblessed1"
+    pass: "whatever your password is"
   }
 });
 
